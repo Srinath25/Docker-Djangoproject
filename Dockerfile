d6d3fde9189d57f361cb3deb679b0e2121228413
Srinath25 <sritharun256@gmail.com>
2024-04-17 03:47:47 -0400

    second commit

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,15 @@ FROM ubuntu
 
 WORKDIR /app
 
-COPY requirements.txt .
+COPY requirements.txt /app
+COPY myweb /app
 
 RUN apt-get update && \
     apt-get install -y python3 python3-pip && \
-    pip install --no-cache-dir -r requirements.txt
+    pip install --no-cache-dir -r requirements.txt && \
+    cd myweb
 
 COPY myweb .
-
-WORKDIR /app/myweb
 
 ENTRYPOINT ["python3", "manage.py"]
 CMD ["runserver", "0.0.0.0:8000"]
--- myweb/manage.py
+++ myweb/manage.py
@@ -3,20 +3,24 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
+    # Print the current Python path for debugging
+    print("Current Python Path:")
+    for path in sys.path:
+        print(path)
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myweb.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        print("ImportError:", exc)
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
